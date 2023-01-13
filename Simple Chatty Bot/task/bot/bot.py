7c84eec7b9f065eddc4cc2032acec8901e8ea01b
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-01-12 18:31:07 -0600

    Initial commit

---
+++ Simple Chatty Bot/task/bot/bot.py
@@ -0,0 +1,7 @@
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+your_name = input()
+print(f'What a great name you have, {your_name}!')
